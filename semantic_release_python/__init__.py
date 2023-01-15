e5b4f155a5c3ad3a6887d2e142efb22c2f768d50
alekseik1 <1alekseik1@gmail.com>
2023-01-15 19:34:57 +0300

    feat(main): min of list

--- semantic_release_python/__init__.py
+++ semantic_release_python/__init__.py
@@ -8,6 +8,17 @@ def min_(a, b):
     return b
 
 
+def min_multi(*args):
+    if len(args) == 0:
+        raise ValueError()
+    if len(args) == 1:
+        return args[0]
+    rv = args[0]
+    for elem in args[1:]:
+        rv = min_(rv, elem)
+    return rv
+
+
 if __name__ == '__main__':
     print("hello")
     print("me")
